bac3e0d2548754430150deca7eb0c09e2dbc5a1c
rivendell <webwyler@gmail.com>
2019-07-21 16:59:50 -0500

    added document.ready function to javascript file

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,49 +1,56 @@
-// Starter array of supers
-var topics = ["wonder woman", "superman", "captain america", "iron man"];
-//supers
+$(document).ready(function(){
+    
+    // Starter array of supers
+    var topics = ["wonder woman", "superman", "captain america", "iron man"];
+    //supers
 
-// Displays super information
-function renderButtons() {
+    // Displays super information
+    function renderButtons() {
 
-    // Deleting the super heros buttons prior to adding new super hero buttons
-    // (this is necessary otherwise we will have repeat buttons)
-    $("#buttons-view").empty();
+        // Deleting the super heros buttons prior to adding new super hero buttons
+        // (this is necessary otherwise we will have repeat buttons)
+        $("#buttons-view").empty();
 
-    // Loop through the array of super heros
-    for (var i = 0; i < topics.length; i++) {
+        // Loop through the array of super heros
+        for (var i = 0; i < topics.length; i++) {
 
-      // Dynamically generate buttons for each super hero in the array.     
-      var super_btn = $("<button>");
+        // Dynamically generate buttons for each super hero in the array.     
+        var super_btn = $("<button>");
 
-      // Add a class
-      super_btn.addClass("super");
+        // Add a class
+        super_btn.addClass("super");
 
-      // Add a data-attribute with a value of the super hero at index i
-      super_btn.attr("data-name", topics[i]);
+        // Add a data-attribute with a value of the super hero at index i
+        super_btn.attr("data-name", topics[i]);
 
-      // Provide the button's text with a value of the super at index i
-      super_btn.text(topics[i]);
+        // Provide the button's text with a value of the super at index i
+        super_btn.text(topics[i]);
 
-      // Add the button to the div I created in my HTML
-      $("#buttons-view").append(super_btn);
+        // Add the button to the div I created in my HTML
+        $("#buttons-view").append(super_btn);
+        }
     }
-  }
 
-  // This function handles events where one button is clicked
-  $("#add-super").on("click", function(event) {
+    // This function handles events where one button is clicked
+    $("#add-super").on("click", function(event) {
 
-    // event.preventDefault() prevents the form from trying to submit itself    
-    event.preventDefault();
+        // event.preventDefault() prevents the form from trying to submit itself    
+        event.preventDefault();
 
-    // Get text from the input box -trim away white space and force lower case
-    var superhero = $("#supers-input").val().trim().toLowerCase();
+        // Get text from the input box -trim away white space and force lower case
+        var superhero = $("#supers-input").val().trim().toLowerCase();
 
-    // The superhero from the textbox is then added to my array
-    topics.push(superhero);
+        // The superhero from the textbox is then added to my array
+        topics.push(superhero);
 
-    // call renderButtons to handle the processing of the superhero array
+        // call renderButtons to handle the processing of the superhero array
+        renderButtons();
+    });
+
+    // Call the renderButtons function at least once to display the initial list of supers
     renderButtons();
-  });
 
-  // Call the renderButtons function at least once to display the initial list of supers
-  renderButtons();
+
+
+});
+
